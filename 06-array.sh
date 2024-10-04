4b5f9f0f8e8a7675fd887cd049c40756a5b8ef40
Sudhirkumar19 <chinna257@gmail.com>
2024-10-05 02:51:13 +0530

    varibles2

---
+++ 06-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Fruits=("Apple", "banana") #Array
+ echo "first fruit is : ${(Fruits[0])}"
+ echo "second fruit is : ${(Fruits[1])}"
+
+ echo "first fruit is : ${(Fruits[@])}" #@ means all fruits at a time
