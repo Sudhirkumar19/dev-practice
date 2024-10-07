649a9888e4faa1e72a241de359563d05c20cab88
Sudhirkumar19 <chinna257@gmail.com>
2024-10-07 23:40:19 +0530

    varibles2

---
+++ 10-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Number=$1
+
+if [$Number -gt 20 ]
+
+then
+    echo " given number : $Number=MBER is greater than 20"
+
+else
+    echo "given number : $Number= IS LESS than 20"
+
+fi 
