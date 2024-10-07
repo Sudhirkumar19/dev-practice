bd7653a9c261f2058501706a16e9e53287b67a5b
Sudhirkumar19 <chinna257@gmail.com>
2024-10-08 00:45:20 +0530

    varibles2

---
+++ 12-functions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "user ID is: $USERID"
+
+VALIDATE(){
+    echo "exit status : $1"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo " please run script with roogt permissions"
+
+    exit 1
+fi
+
+dnf list installed git
+
+VALIDATE $?
+
+# if [ $? -ne 0 ]
+# then
+#     echo " git is not installed, going to install it..."
+#     dnf install git -y
+#     if [ $? -ne 0]
+#     then
+#         echo "git installation is not done"
+#         exit 1
+#     else
+#         echo "git installation is done"
+#     fi
+
+
+# else 
+#     echo " git already installed, nothing to do..."
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then
+#     echo "MYSQL is not installed"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then 
+#         echo " MYSQL installation is failure"
+#         exit 1
+
+#     else 
+#         echo "MYSQL installtion is success"
+#     fi
+# else
+#     echo "MUSQL is already installed"
+# fi
